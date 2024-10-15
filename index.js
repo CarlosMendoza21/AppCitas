af2450005bb83a01eab42313e524ed1019deba3c
OscarRamirez88 <oramirez08@alumnos.uaq.mx>
2024-10-15 15:45:09 -0600

    Primer Commit App Start

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-/* Hola sebas */
+let currentIndex = 0;
+
+    function moveCarousel(direction) {
+      const items = document.querySelectorAll('.carousel-item');
+      const totalItems = items.length;
+      currentIndex = (currentIndex + direction + totalItems) % totalItems;
+      const offset = -currentIndex * 100;
+      items.forEach(item => {
+        item.style.transform = `translateX(${offset}%)`;
+      });
+    }
